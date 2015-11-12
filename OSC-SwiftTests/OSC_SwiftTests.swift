a8d230b298e23ffc30eeef2433bf0a9b08518bb4
DingJianhua <1026699127@qq.com>
2015-11-12 17:41:51 +0800

    Initial Commit

---
+++ OSC-SwiftTests/OSC_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  OSC_SwiftTests.swift
+//  OSC-SwiftTests
+//
+//  Created by DingJH on 15/11/12.
+//  Copyright © 2015年 Ding. All rights reserved.
+//
+
+import XCTest
+@testable import OSC_Swift
+
+class OSC_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
